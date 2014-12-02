7dbb2dabc70ec9fdaf7824e73279a3f1fd0c1c2f
Jeremy Castagno <JeremyBYU@gmail.com>
2014-12-02 13:46:42 -0600

    ASCII CAM Working
    Got the ASCII CAM Working.  Works on mobile and desktop devices.
    Discoverd that the width and height variabled in asciicam.js are vitally
    imporant.They are set to 80 and 60 pixels respectively but each pixel is
    represented by a character which actually taxes up more than one pixel,
    hence the display to the user will be much more than that, about 6 times
    actually.

---
+++ client/main.init.js
@@ -0,0 +1,37 @@
+/* off-canvas sidebar toggle */
+
+
+$(document).ready(init);
+
+if (Meteor.isCordova) {
+    document.addEventListener("deviceready", onDeviceReady, false);
+}
+
+// Handler for .ready() called.
+
+
+
+function onDeviceReady() {
+    init();
+
+}
+
+function sideBarInit() {
+    $('[data-toggle=offcanvas]').click(function() {
+        $(this).toggleClass('visible-xs text-center');
+        $(this).find('i').toggleClass('glyphicon-chevron-right glyphicon-chevron-left');
+        $('.row-offcanvas').toggleClass('active');
+        $('#lg-menu').toggleClass('hidden-xs').toggleClass('visible-xs');
+        $('#xs-menu').toggleClass('visible-xs').toggleClass('hidden-xs');
+        $('#btnShow').toggle();
+    });
+
+
+}
+function init()
+{
+  sideBarInit();
+  app.init();
+
+
+}
